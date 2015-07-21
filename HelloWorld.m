a828f908bddd6a42deb1c2a131f43257f3020db5
typhoonday <7500994@qq.com>
2015-07-21 23:03:00 +0800

    加两个文件

---
+++ HelloWorld.m
@@ -0,0 +1,10 @@
+#import <Foundation/Fundation.h>
+//定义main方法，作为程序入口
+int main(int argc, char *argv[])
+{
+	@autoreleasepool
+	{
+		NSLog(@“Hello Objective-C”);
+	}
+	return 0;
+}
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by 董涛 on 15/7/18.
+//  Copyright (c) 2015年 dongtao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
