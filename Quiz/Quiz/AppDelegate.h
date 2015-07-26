09e2a7294690a7107a2a101dc7054bb1dfc8e64b
typhoonday <7500994@qq.com>
2015-07-26 22:23:01 +0800

    QUIZ

---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by 董涛 on 15/7/26.
+//  Copyright (c) 2015年 DONG TAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
